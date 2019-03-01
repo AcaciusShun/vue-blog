10acb668b9697532a5907e1d32785c8956363ed8
bailongctui <bailongctui@gmail.com>
2019-03-01 17:07:40 +0800

    init

---
+++ client/src/components/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+// $blue: red;
+//   .lists {
+//     color: $blue;
+//     .items {
+//       font-size: 20px
+//     }
+//   }
+
+</style>

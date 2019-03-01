2e9239440872e713559f619906b0d5e9740d1dc1
bailongctui <bailongctui@gmail.com>
2019-03-01 17:08:42 +0800

    引入sass \ ajax

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 引入 axios
+import axios from 'axios'
+Vue.prototype.$ajax=axios;
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

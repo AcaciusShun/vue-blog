9207fb8e8ad465c27fbe6509f38c4b2f7c3c0c60
bailongctui <bailongctui@gmail.com>
2019-03-01 17:09:09 +0800

    server router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Demo from '@/components/Demo'
+import Login from '@/pages/login/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Demo',
+      component: Demo
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/admin', function(req, res,next) {
+  console.log(req)
+  var result = {
+      code : 1,
+      messages : "登录成功"
+  };
+    res.json(result);
+})
+router.post('/admin', function(req, res,next) {
+  console.log(req)
+
+  var result = {
+      code : 1,
+      messages : "登录成功"
+  };
+    res.json(result);
+})
+
 
 module.exports = router;
